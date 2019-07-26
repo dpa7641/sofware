4d693a1bd317347af344f70e6dec100036429451
jorgedq <jorgedq7952@gmail.com>
2019-07-25 22:18:49 -0400

    sidebar

--- teach-me/src/components/Sidebar.js
+++ teach-me/src/components/Sidebar.js
@@ -1,6 +1,17 @@
 import React from "react";
 import './Sidebar.css';
 
-const Sidebar = () => (<div className="col-md-3" />);
+const Sidebar = () => (
+    <div className="col-md-3">
+        <div className="sidebar">
+            <a href="" className=" btn-primary btn-block">LISTA DE USUARIOS</a>
+            <a href="" className=" btn-primary btn-block">REGITRO DE USUARIOS</a>
+            <a href="" className=" btn-primary btn-block">REGISTRO DE ROL</a>
+            <a href="" className=" btn-primary btn-block">REGISTRO PERMISO</a>
+            <a href="" className=" btn-primary btn-block">ASIGNAR PERMISO</a>
+            <a href="roles.html" className=" btn-primary btn-block">ASIGNAR ROL</a>
+        </div>
+    </div>
+    );
 
 export default Sidebar;
