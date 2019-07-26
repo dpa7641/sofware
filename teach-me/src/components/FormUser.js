06160f21e11ed1727595840d8392ec1d98c60b9a
jorgedq <jorgedq7952@gmail.com>
2019-07-26 03:00:27 -0400

    agregado FormUser.js formulario para creacion de usuarios

---
+++ teach-me/src/components/FormUser.js
@@ -0,0 +1,90 @@
+import React,{Component} from 'react';
+
+
+class FormUser extends Component{
+    constructor(props){
+        super(props);
+        this.state= {
+            roles:[],
+            nombre: '',
+            apPaterno: '',
+            apMaterno: '',
+            ci: '',
+            email: '',
+            usuario:'',
+            contrasenia: '',
+            vcontrasenia: '',
+            rol: ''
+        }
+    }
+
+    componentDidMount(){
+        fetch("http://localhost:8090/roles")
+            .then(res => res.json())
+            .then(json => {
+                this.setState({roles: json})
+            });
+    }
+
+    handleChange = (event) => {
+        this.setState({[event.target.name]:event.target.value})
+    }
+
+    handleSubmit = (event) => {
+        alert('you submited text data: '+this.state.textData);
+    }
+
+    render(){
+        const rolList = this.state.roles.map((rol, index) =>
+            <option value={rol.idRol} key={index}>{rol.descripcion}</option>
+        );
+        return(
+            <div className="col-md-5">
+                <h1>Registro de usuario</h1>
+                <form onSubmit={this.handleSubmit} clasName="inputForm" >
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Nombre</label>
+                        <input name="nombre" onChange={this.handleChange} className="form-control"id="exampleInputEmail1" placeholder="Nombre..." required/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Apellido Paterno</label>
+                        <input name="apPaterno" onChange={this.handleChange} type="text" className="form-control" id="exampleInputEmail1" placeholder="Apellido Paterno..." required/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Apellido Materno</label>
+                        <input name="apMaterno" onChange={this.handleChange} type="text" className="form-control" id="exampleInputEmail1" placeholder="Apellido Materno..." required/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Cedula Identidad</label>
+                        <input name="ci" onChange={this.handleChange} type="text" className="form-control" id="exampleInputEmail1" placeholder="Cedula Identidad..." required/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">usuario</label>
+                        <input name="usuario" onChange={this.handleChange} type="text" className="form-control" id="exampleInputEmail1" placeholder="Cedula Identidad..." required/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Email</label>
+                        <input name="email" onChange={this.handleChange} type="email" className="form-control" id="exampleInputEmail1" placeholder="Email..." required/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Contraseña</label>
+                        <input name="contrasenia" onChange={this.handleChange} type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" required/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Repita Contraseña</label>
+                        <input name="vcontrasenia" onChange={this.handleChange} type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Seleccione Rol</label>
+                        <select name="rol" onChange={this.handleChange} className="form-control">
+                            {rolList}
+                        </select>
+                    </div>
+                    <button type="submit" className="mt-4 btn btn-success">CREAR</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default FormUser;
